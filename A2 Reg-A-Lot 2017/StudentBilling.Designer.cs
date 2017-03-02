6afba1346a999941deccf400967664e391c4820f
Ttano <nattoup24@gmail.com>
2017-03-01 18:27:35 -0600

    Changed some textbox name properties in Student Billing form (txtName) (txtPhone) (txtTotalFees) etc.

--- A2 Reg-A-Lot 2017/StudentBilling.Designer.cs
+++ A2 Reg-A-Lot 2017/StudentBilling.Designer.cs
@@ -31,22 +31,22 @@
             this.btnBack = new System.Windows.Forms.Button();
             this.label11 = new System.Windows.Forms.Label();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerPaymentDue = new System.Windows.Forms.DateTimePicker();
             this.label6 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.txtTotalFees = new System.Windows.Forms.TextBox();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numUpDownCourses = new System.Windows.Forms.NumericUpDown();
             this.txtPhone = new System.Windows.Forms.MaskedTextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownCourses)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBack
@@ -71,10 +71,10 @@
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.dateTimePicker1);
+            this.groupBox2.Controls.Add(this.dateTimePickerPaymentDue);
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Controls.Add(this.label4);
-            this.groupBox2.Controls.Add(this.textBox3);
+            this.groupBox2.Controls.Add(this.txtTotalFees);
             this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.groupBox2.Location = new System.Drawing.Point(21, 320);
             this.groupBox2.Name = "groupBox2";
@@ -83,13 +83,13 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Fees Info";
             // 
-            // dateTimePicker1
+            // dateTimePickerPaymentDue
             // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(116, 89);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
-            this.dateTimePicker1.TabIndex = 3;
+            this.dateTimePickerPaymentDue.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerPaymentDue.Location = new System.Drawing.Point(116, 89);
+            this.dateTimePickerPaymentDue.Name = "dateTimePickerPaymentDue";
+            this.dateTimePickerPaymentDue.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerPaymentDue.TabIndex = 3;
             // 
             // label6
             // 
@@ -111,23 +111,23 @@
             this.label4.TabIndex = 1;
             this.label4.Text = "Total Fees";
             // 
-            // textBox3
+            // txtTotalFees
             // 
-            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(116, 49);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(200, 20);
-            this.textBox3.TabIndex = 0;
+            this.txtTotalFees.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTotalFees.Location = new System.Drawing.Point(116, 49);
+            this.txtTotalFees.Name = "txtTotalFees";
+            this.txtTotalFees.Size = new System.Drawing.Size(200, 20);
+            this.txtTotalFees.TabIndex = 0;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.numUpDownCourses);
             this.groupBox1.Controls.Add(this.txtPhone);
             this.groupBox1.Controls.Add(this.label5);
             this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.txtAddress);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.txtName);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(21, 46);
             this.groupBox1.Name = "groupBox1";
@@ -135,12 +135,12 @@
             this.groupBox1.TabIndex = 71;
             this.groupBox1.TabStop = false;
             // 
-            // numericUpDown1
+            // numUpDownCourses
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(116, 215);
-            this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(169, 20);
-            this.numericUpDown1.TabIndex = 49;
+            this.numUpDownCourses.Location = new System.Drawing.Point(116, 215);
+            this.numUpDownCourses.Name = "numUpDownCourses";
+            this.numUpDownCourses.Size = new System.Drawing.Size(169, 20);
+            this.numUpDownCourses.TabIndex = 49;
             // 
             // txtPhone
             // 
@@ -168,13 +168,13 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Phone Number";
             // 
-            // textBox2
+            // txtAddress
             // 
-            this.textBox2.Location = new System.Drawing.Point(116, 123);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(169, 68);
-            this.textBox2.TabIndex = 3;
+            this.txtAddress.Location = new System.Drawing.Point(116, 123);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(169, 68);
+            this.txtAddress.TabIndex = 3;
             // 
             // label2
             // 
@@ -185,12 +185,12 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Student Address";
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Location = new System.Drawing.Point(116, 30);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(169, 20);
-            this.textBox1.TabIndex = 1;
+            this.txtName.Location = new System.Drawing.Point(116, 30);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(169, 20);
+            this.txtName.TabIndex = 1;
             // 
             // label1
             // 
@@ -216,7 +216,7 @@
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownCourses)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -227,18 +227,18 @@
         private System.Windows.Forms.Button btnBack;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.GroupBox groupBox2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePickerPaymentDue;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox txtTotalFees;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numUpDownCourses;
         private System.Windows.Forms.MaskedTextBox txtPhone;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtAddress;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label label1;
     }
 }
